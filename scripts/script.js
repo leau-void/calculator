956c58c4c930dc16b85e9fd57c7d471981d08268
leau-void <lpierra@gmail.com>
2021-07-23 14:42:28 -0300

    now working on keyboard support

--- scripts/script.js
+++ scripts/script.js
@@ -1,11 +1,10 @@
 let display = document.querySelector("#display");
 let container = document.querySelector("#container");
+let keyboardGuide = document.querySelector("#keyboard");
 let currentOperator;
 let lastOperator;
 let currentNumber = 0;
 let lastNumber;
-
-
 
 container.addEventListener("click", () => {
   let target = event.target;
@@ -18,24 +17,23 @@ container.addEventListener("click", () => {
     displayNum(target.value);
 
   } else if (target.classList.contains("operators")) {
+
+    if (!Number(display.textContent)) return;
     
-  currentNumber = Number(display.textContent);
+    currentNumber = Number(display.textContent);
     if (currentOperator == "equal") currentOperator = null;
 
     lastOperator = currentOperator;
     currentOperator = target.id;
     if (lastOperator) {
       
-      //new not sure!!
       if (lastNumber == null || lastNumber == undefined) {
         lastNumber = currentNumber;
         currentNumber = Number(display.textContent);
         }
-      //
       
       displayResult(operate(lastOperator, lastNumber, currentNumber));
       lastOperator = null;
-      //not sure
       lastNumber = null;
     }
     
@@ -56,15 +54,29 @@ container.addEventListener("click", () => {
     currentNumber = null;
 
   } else if (target.id == "plus-minus") {
+    if (display.textContent === "0") return;
+    if (!Number(display.textContent)) return;
     if (display.textContent.indexOf("-") != -1) {
       display.textContent = display.textContent.substring(1);
     } else display.textContent = "-" + display.textContent;
 
   } else if (target.id == "percentage") {
-    
+    if (!Number(display.textContent)) return;
+    if (display.textContent.length > 7) return;
+    display.textContent = (Number(display.textContent) / 100);
+
+  } else if (target.id == "backspace") {
+    display.textContent = display.textContent.slice(0, -1);
   }
 })
 
+keyboardGuide.addEventListener("click", () => {
+  if (keyboardGuide.classList.contains("toggle-active")) {
+    keyboardGuide.classList.remove("toggle-active");
+  } else {
+    keyboardGuide.classList.add("toggle-active");
+  }
+})
 
 function displayNum(value) {
  
@@ -95,7 +107,13 @@ function displayResult(result) {
 
   resultAsString = result.toString();
   resultLen = resultAsString.length;
-  if (resultLen > 8) result = "Sorry..."
+  if (resultLen > 8) {
+    result = "Sorry..."
+    currentNumber = null;
+    lastNumber = null;
+    currentOperator = null;
+    lastOperator = null;
+  }
   display.textContent = result;
 }
 
@@ -118,7 +136,6 @@ function operate(operator, num1, num2) {
       output = divide(num1, num2);
       break;
   }
-  console.log(output);
   return output;
 }
 
